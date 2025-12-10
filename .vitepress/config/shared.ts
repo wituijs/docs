0cc4897adc6752720e4b331d2b4963c2b8f66a5d
yangpu <sunya123456>
2025-12-10 08:33:24 +0800

    ;

--- .vitepress/config/shared.ts
+++ .vitepress/config/shared.ts
@@ -1,50 +1,50 @@
-import { defineConfig, HeadConfig } from 'vitepress'
-import { zhSearch } from './zh'
+import { defineConfig, HeadConfig } from "vitepress";
+import { zhSearch } from "./zh";
 
-export const META_IMAGE = ''
+export const META_IMAGE = "";
 export const isProduction =
-  process.env.NETLIFY && process.env.CONTEXT === 'production'
+  process.env.NETLIFY && process.env.CONTEXT === "production";
 
 if (process.env.NETLIFY) {
-  console.log('Netlify build', process.env.CONTEXT)
+  console.log("Netlify build", process.env.CONTEXT);
 }
 
-const productionHead: HeadConfig[] = []
+const productionHead: HeadConfig[] = [];
 
-const rControl = /[\u0000-\u001f]/g
-const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'“”‘’<>,.?/]+/g
-const rCombining = /[\u0300-\u036F]/g
+const rControl = /[\u0000-\u001f]/g;
+const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'“”‘’<>,.?/]+/g;
+const rCombining = /[\u0300-\u036F]/g;
 
 /**
  * Default slugification function
  */
 export const slugify = (str: string): string =>
   str
-    .normalize('NFKD')
+    .normalize("NFKD")
     // Remove accents
-    .replace(rCombining, '')
+    .replace(rCombining, "")
     // Remove control characters
-    .replace(rControl, '')
+    .replace(rControl, "")
     // Replace special characters
-    .replace(rSpecial, '-')
+    .replace(rSpecial, "-")
     // ensure it doesn't start with a number
-    .replace(/^(\d)/, '_$1')
+    .replace(/^(\d)/, "_$1");
 
 export const sharedConfig = defineConfig({
   // base: "./",
-  outDir: 'dist',
-  title: 'Wit-ui',
-  appearance: 'dark',
+  outDir: "dist",
+  title: "Wit-ui",
+  appearance: "dark",
 
   markdown: {
     theme: {
-      dark: 'dracula-soft',
-      light: 'vitesse-light',
+      dark: "dracula-soft",
+      light: "vitesse-light",
     },
 
     attrs: {
-      leftDelimiter: '%{',
-      rightDelimiter: '}%',
+      leftDelimiter: "%{",
+      rightDelimiter: "}%",
     },
 
     anchor: {
@@ -53,49 +53,43 @@ export const sharedConfig = defineConfig({
   },
 
   head: [
-    ['link', { rel: 'icon', type: 'image/png', href: '/logo.png' }],
-    ['link', { rel: 'icon', type: 'image/png', href: '/logo.png' }],
+    ["link", { rel: "icon", type: "image/png", href: "/logo.png" }],
+    ["link", { rel: "icon", type: "image/png", href: "/logo.png" }],
 
     [
-      'meta',
-      { name: 'wwads-cn-verify', content: '5878a7ab84fb43402106c575658472fa' },
+      "meta",
+      { name: "wwads-cn-verify", content: "5878a7ab84fb43402106c575658472fa" },
     ],
 
     [
-      'meta',
+      "meta",
       {
-        property: 'og:type',
-        content: 'website',
+        property: "og:type",
+        content: "website",
       },
     ],
 
     [
-      'meta',
+      "meta",
       {
-        property: 'twitter:card',
-        content: 'summary_large_image',
+        property: "twitter:card",
+        content: "summary_large_image",
       },
     ],
     [
-      'meta',
+      "meta",
       {
-        property: 'twitter:image',
+        property: "twitter:image",
         content: META_IMAGE,
       },
     ],
 
     // 爱站统计
     [
-      'script',
+      "script",
       {},
       `
-      var _mtj = _mtj || [];
-      (function () {
-      var mtj = document.createElement("script");
-      mtj.src = "https://node68.aizhantj.com:21233/tjjs/?k=q8m2dyyx858";
-      var s = document.getElementsByTagName("script")[0];
-      s.parentNode.insertBefore(mtj, s);
-      })();
+      !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3OJP1VqIt9Kt7AgH",ck:"3OJP1VqIt9Kt7AgH"});
       `,
     ],
 
@@ -103,14 +97,14 @@ export const sharedConfig = defineConfig({
   ],
 
   themeConfig: {
-    logo: '/logo.png',
+    logo: "/logo.png",
     outline: [2, 3],
 
     socialLinks: [
       // { icon: 'x', link: 'https://twitter.com/posva' },
       {
-        icon: 'github',
-        link: 'https://github.com/wituijs/docs',
+        icon: "github",
+        link: "https://github.com/wituijs/docs",
       },
       // {
       //   icon: 'discord',
@@ -119,29 +113,29 @@ export const sharedConfig = defineConfig({
     ],
 
     footer: {
-      copyright: 'Copyright © 2024 Wit-ui All Rights Reserved',
-      message: '湘ICP备2024070110号',
+      copyright: "Copyright © 2024 Wit-ui All Rights Reserved",
+      message: "湘ICP备2024070110号",
     },
 
     editLink: {
-      pattern: 'https://github.com/wituijs/docs',
-      text: 'Suggest changes',
+      pattern: "https://github.com/wituijs/docs",
+      text: "Suggest changes",
     },
 
     search: {
-      provider: 'algolia',
+      provider: "algolia",
       options: {
-        appId: 'JBX2ZJ89K9',
-        apiKey: '78a0466ad2b7506c2112531f76b2a921',
-        indexName: 'wit-ui',
+        appId: "JBX2ZJ89K9",
+        apiKey: "78a0466ad2b7506c2112531f76b2a921",
+        indexName: "wit-ui",
         locales: { ...zhSearch },
       },
     },
 
     carbonAds: {
-      code: 'CEBICK3I',
+      code: "CEBICK3I",
       // custom: 'CEBICK3M',
-      placement: 'routervuejsorg',
+      placement: "routervuejsorg",
     },
   },
-})
+});
